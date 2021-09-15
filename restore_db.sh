1b33da2e18db9d9292c59789cb634f967cfc306e
Dan Overholt <vcicodehub@gmail.com>
2021-09-15 11:33:58 -0400

    testing sh script

--- restore_db.sh
+++ restore_db.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-echo "Creating rmm databases..."
-su postgres -c 'createdb rmm_orders'
-su postgres -c 'createdb rmm_users'
-su postgres -c 'createdb rmm_shops'
-su postgres -c 'createdb rmm_vendors'
-su postgres -c 'createdb rmm_events'
 echo "Restoring rmm databases from dump files..."
-su postgres -c 'psql rmm_orders < /var/lib/postgresql/rmm_orders.db.dat'
-su postgres -c 'psql rmm_users < /var/lib/postgresql/rmm_users.db.dat'
-su postgres -c 'psql rmm_shops < /var/lib/postgresql/rmm_shops.db.dat'
-su postgres -c 'psql rmm_vendors < /var/lib/postgresql/rmm_vendors.db.dat'
-su postgres -c 'psql rmm_events < /var/lib/postgresql/rmm_events.db.dat'
+su postgres -c 'psql rmm_orders -f /var/lib/postgresql/rmm_orders.db.dat'
+su postgres -c 'psql rmm_users -f /var/lib/postgresql/rmm_users.db.dat'
+su postgres -c 'psql rmm_shops -f /var/lib/postgresql/rmm_shops.db.dat'
+su postgres -c 'psql rmm_vendors -f /var/lib/postgresql/rmm_vendors.db.dat'
+su postgres -c 'psql rmm_events -f /var/lib/postgresql/rmm_events.db.dat'
 echo "Restore complete."
