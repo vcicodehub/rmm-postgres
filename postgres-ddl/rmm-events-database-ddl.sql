4470c70b77c72a5a22ffc9cf9490bb42313a81ae
Dan Overholt <vcicodehub@gmail.com>
2021-09-16 17:00:02 -0400

    Added lable column to rmm_events

--- postgres-ddl/rmm-events-database-ddl.sql
+++ postgres-ddl/rmm-events-database-ddl.sql
@@ -5,6 +5,7 @@ create table "rmm_events" (
   "e_type" varchar(500),
   "e_type_id" decimal,
   "e_title" varchar(500),
+  "e_label" varchar(500),
   "e_description" varchar(4000),
   "e_add_user_id" varchar(500),
   "e_add_date" date,
