0b80e5c4afa4031e51413fcd6396f4d3fc8c3924
Overholt, Dan <Daniel.Overholt@SignetJewelers.com>
2021-10-27 09:36:11 -0400

    Added additional db updates

--- postgres-dump/rmm-update.sh
+++ postgres-dump/rmm-update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 su postgres -c 'psql rmm_vendors -f /var/lib/postgresql/rmm-vendors-update-02-script.sql'
-su postgres -c 'psql rmm_vendors -f /var/lib/postgresql/rmm-orders-update-script.sql'
+su postgres -c 'psql rmm_orders -f /var/lib/postgresql/rmm-orders-update-script.sql'
